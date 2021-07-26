b8e6f57e4d0ab796d5668b6b0710c56e1b7dca8a
andy <a.courtade.lebas@gmail.com>
2021-07-26 16:19:38 +0200

    classe console modifier

--- src/main/java/metier/Console.java
+++ src/main/java/metier/Console.java
@@ -1,25 +1,71 @@
 package metier;
 
+import java.time.LocalDate;
+
 public class Console {
 
 	private String nom;
+    private int prix;
+    private LocalDate sortie;  
+    
+	
 
-	public Console(String nom) {
-		this.nom = nom;
-	}
-
+	
 	public String getNom() {
 		return nom;
 	}
+
+
+
 
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
 
+
+
+
+	public int getPrix() {
+		return prix;
+	}
+
+
+
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+
+
+
+	public LocalDate getSortie() {
+		return sortie;
+	}
+
+
+
+
+	public void setSortie(LocalDate sortie) {
+		this.sortie = sortie;
+	}
+
+
+
+
+	public Console(String nom, int prix, LocalDate sortie) {
+		this.nom = nom;
+		this.prix = prix;
+		this.sortie = sortie;
+	}
+
+
+
+
 	@Override
 	public String toString() {
-		return "Console [nom=" + nom + "]";
+		return "Console [nom=" + nom + ", prix=" + prix + ", sortie=" + sortie + "]";
 	}
-	
+
 	
 }
