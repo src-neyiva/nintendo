636138758f54931a2af7a60a2b6cc6744c56fbbc
andy <a.courtade.lebas@gmail.com>
2021-07-26 16:03:16 +0200

    ajout de achat(classe)

---
+++ src/main/java/metier/Achat.java
@@ -0,0 +1,48 @@
+package metier;
+
+import java.time.LocalDate;
+
+public class Achat {
+
+	private Jeu jeu;
+	private LocalDate date;
+	private int prix;
+	private Boutique boutique;
+	public Achat(Jeu jeu, LocalDate date, int prix, Boutique boutique) {
+		this.jeu = jeu;
+		this.date = date;
+		this.prix = prix;
+		this.boutique = boutique;
+	}
+	public Jeu getJeu() {
+		return jeu;
+	}
+	public void setJeu(Jeu jeu) {
+		this.jeu = jeu;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public int getPrix() {
+		return prix;
+	}
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+	public Boutique getBoutique() {
+		return boutique;
+	}
+	public void setBoutique(Boutique boutique) {
+		this.boutique = boutique;
+	}
+	@Override
+	public String toString() {
+		return "Achat [jeu=" + jeu + ", date=" + date + ", prix=" + prix + ", boutique=" + boutique + "]";
+	}
+	
+	
+	
+}
